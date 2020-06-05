dfa76e568116942262bcad04f4a7ca278e04e701
nataliiashavshyna <shavshyna@gmail.com>
2020-06-05 16:47:06 +0300

    JCSP-381: review comments fixes

--- custom-vu/src/main/kotlin/jces1209/vu/page/AttachScreenShot.kt
+++ custom-vu/src/main/kotlin/jces1209/vu/page/AttachScreenShot.kt
@@ -5,22 +5,41 @@ import org.openqa.selenium.*
 import org.openqa.selenium.interactions.Actions
 import org.openqa.selenium.support.ui.ExpectedConditions
 
-abstract class AttachScreenShot(
-    protected val driver: WebDriver
+open class AttachScreenShot(
+    protected val driver: WebDriver,
+    private var countBefore: Int
 ) {
-
     fun makeScreenShot() {
-        val src = (driver as TakesScreenshot).getScreenshotAs(OutputType.BYTES)
+        (driver as TakesScreenshot).getScreenshotAs(OutputType.BYTES)
     }
 
     fun pasteScreenShot() {
-        val builder = Actions(driver)
-        builder.keyDown(Keys.CONTROL).sendKeys("v").perform()
+        countBefore = getIssueScreenShotsCount(By.xpath("(//*[contains(@class,'overlay image persistent sc-lbihag')])"))
+        Actions(driver).keyDown(Keys.CONTROL).sendKeys("v").perform()
     }
 
     fun waitForTheScreenShotAttached() {
-        driver.wait(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[@data-test-id=" +
-            "'issue.views.issue-base.content.attachment.filmstrip-panel']" +
-            "//span[contains(text(),'Attachments')])")))
+        driver.wait(ExpectedConditions.numberOfElementsToBeMoreThan(
+            By.xpath("(//*[contains(@class,'overlay image persistent sc-lbihag')])"), countBefore))
+        driver.wait(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("(//*[@data-test-id=" +
+                "'issue.views.issue-base.content.attachment.filmstrip-panel']" +
+                "//span[contains(text(),'Attachments')])")))
+        waitForAttributeValueForElements(By.xpath(
+            "//*[@data-testid='media-file-card-view']"),
+            "data-test-status",
+            "complete")
+        val after = getIssueScreenShotsCount(By.xpath("(//*[contains(@class,'overlay image persistent sc-lbihag')])"))
+    }
+
+    private fun waitForAttributeValueForElements(by: By, attribute: String, expected: String) {
+        val elements = driver.findElements(by)
+        elements.forEach {
+            driver.wait(ExpectedConditions.attributeContains(it, attribute, expected))
+        }
+    }
+
+    fun getIssueScreenShotsCount(by: By): Int {
+        return driver.findElements(by).size
     }
 }
